6c91209e86e7173413e0120fbc97e7a10304ae59
jxa98 <jxa98@sfu.ca>
2025-03-10 01:11:06 -0700

    Initial commit

---
+++ BlindAssist/BlindAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindAssistApp.swift
+//  BlindAssist
+//
+//  Created by chongyuyuan on 2025-03-10.
+//
+
+import SwiftUI
+
+@main
+struct BlindAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindAssist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BlindAssist
+//
+//  Created by chongyuyuan on 2025-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
